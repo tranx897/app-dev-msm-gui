ea2d56424c9a6af6af042fcc3e472399d02c9677
tranx897 <63356332+tranx897@users.noreply.github.com>
2024-05-01 16:01:05 +0000

    Add Create Movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,22 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  def create
+    # params hash looks like this: 
+    # {"the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"6"}
+    # Retrieve user's input from params
+    m = Movie.new
+    m.title = params["the_title"]
+    m.year = params["the_year"]
+    m.duration = params["the_duration"]
+    m.description = params["the_description"]
+    m.image = params["the_image"]
+    m.director_id = params["the_director_id"]
+
+    m.save
+
+    # Redirect the user back to the /movies URL
+    redirect_to("/movies")
+  end
 end
