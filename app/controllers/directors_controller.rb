ec5d354e75e75d9f9fe5cd1a41cf7025f18d8672
tranx897 <63356332+tranx897@users.noreply.github.com>
2024-05-05 05:06:41 +0000

    Add form to create, update, destroy directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -36,4 +36,41 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/eldest" })
   end
+
+  def create
+    d = Director.new
+    d.name = params["name"]
+    d.dob = params["dob"]
+    d.bio = params["bio"]
+    d.image = params["image"]
+
+    d.save
+
+    # Redirect the user back to the /directors URL
+    redirect_to("/directors")
+  end
+
+  def destroy
+    director_id = params["director_id"]
+    matching_records = Director.where({:id => director_id})
+    the_director = matching_records[0]
+    the_director.destroy
+
+    redirect_to("/directors")
+  end
+  
+  def update
+    director_id = params["director_id"]
+    matching_records = Director.where({ :id => director_id})
+    d = matching_records[0]
+
+    d.name = params["name"]
+    d.dob = params["dob"]
+    d.bio = params["bio"]
+    d.image = params["image"]
+
+    d.save
+
+    redirect_to("/directors/#{d.id}")
+  end
 end
